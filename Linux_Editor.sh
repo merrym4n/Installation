760b3c4403cf6d7f52f6c6326064bbac12061df2
merrym4n <merrym4n@gmai.com>
2018-03-13 00:17:03 +0900

    linux_editor

---
+++ Linux_Editor.sh
@@ -0,0 +1,22 @@
+#install sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+#install typora
+sudo apt-get adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+sudo add-apt-repository 'deb http://typora.io linux/'
+sudo apt-get update
+sudo apt-get install typora
+
+#install vim
+sudo apt-get install vim
+
+#up-to-date
+sudo apt-get update
+sudo apt-get upgrade
+
+:<<'COMMENT'
+COMMENT
